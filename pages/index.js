d5b91ee1ca4554f859f7bea20aecef87874e35a8
intro-taktak <tak@introsolution.kr>
2021-04-06 15:02:20 +0900

    로고추가

--- pages/index.js
+++ pages/index.js
@@ -22,12 +22,12 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="introsolution.kr"
           target="_blank"
           rel="noopener noreferrer"
         >
           ⓒ{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          <img src="/logo.png" alt="Intro Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
