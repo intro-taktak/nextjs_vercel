621fe544708bc7b27b67cc5fe5d4eceafdeeaa6c
intro-taktak <tak@introsolution.kr>
2021-04-06 16:12:24 +0900

    머티리얼 추가

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,25 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Button from '@material-ui/core/Button';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Intro Solution</title>
+        <title>Intro</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://introsolution.kr">Intro Solution</a>
+          Welcome to <a href="https://introsolution.kr">Intro</a>
         </h1>
 
         <p className={styles.description}>
-          인트로솔루션{' '}
+          인트로{' '}
           <code className={styles.code}>페이지 준비중입니다.</code>
         </p>
+        <Button variant="contained">Hello World</Button>
       </main>
 
       <footer className={styles.footer}>
