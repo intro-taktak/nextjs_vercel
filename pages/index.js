03523b791b446fdf9cbf4a2a2f198e04e2b86f6d
intro-taktak <tak@introsolution.kr>
2021-04-07 14:36:56 +0900

    구조 재구성

--- pages/index.js
+++ pages/index.js
@@ -1,56 +1,52 @@
-import Head from 'next/head'
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import styles from '../styles/Home.module.css'
-import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
 import Fab from '@material-ui/core/Fab';
-import SendIcon from '@material-ui/icons/Send';
+import NavigationIcon from '@material-ui/icons/Navigation';
 
 const useStyles = makeStyles((theme) => ({
-  fabDial: {
-      position: 'absolute',
-      bottom: theme.spacing(3),
-      right: theme.spacing(3),
+  fab: {
+    position: 'absolute',
+    bottom: theme.spacing(3),
+    right: theme.spacing(3),
   },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+  logo: {
+    height:150
+  }
 }));
 
 
-export default function Home() {
+export default function Index() {
   const classes = useStyles();
 
   return (
-    <div>
-      <Head>
-        <title>Intro</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+    <Container maxWidth="sm">
+      <h1>
           Welcome to <a href="https://introsolution.kr">Intro</a>
         </h1>
 
-        <p className={styles.description}>
+        <p>
           인트로{' '}
-          <code className={styles.code}>페이지 준비중입니다.</code>
+          <code>페이지 준비중입니다.</code>
         </p>
-        <Button variant="contained">Hello World</Button>
-      </main>
-
-      <Fab variant="extended" className={classes.fabDial}>
-        <SendIcon></SendIcon>
-        문의하기
-      </Fab>
-
-      <footer className={styles.footer}>
-        <a
-          href="/"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Fab
+          variant="extended"
+          size="medium"
+          color="primary"
+          aria-label="add"
+          className={classes.fab}
         >
-          ⓒ{' '}
-          <img src="/logo.png" alt="Intro Logo" className={styles.logo} />
-        </a>
+          <NavigationIcon className={classes.extendedIcon} />
+          문의하기
+        </Fab>
+        <footer>
+        <img src="/logo.png" alt="Intro Logo" className={classes.logo} />
       </footer>
-    </div>
-  )
+    </Container>
+    
+    
+  );
 }
