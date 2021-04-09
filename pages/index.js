9f465bd6a47f3f6556172893b2ac31f95d8dc581
tak@introsolution.kr <jytlove118@naver.com>
2021-04-09 16:18:02 +0900

    반응형 폰트 사이즈

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, createMuiTheme, responsiveFontSizes, ThemeProvider } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import ContactForm from '../src/ContactForm'
 import Typography from '@material-ui/core/Typography';
@@ -15,19 +15,21 @@ const useStyles = makeStyles((theme) => ({
   },
   typo:{
     marginBottom: 22,
-    fontWeight: 800
+    fontWeight: 800,
   }
 }));
 
 
 export default function Index() {
   const classes = useStyles();
+  let theme = createMuiTheme();
+  theme = responsiveFontSizes(theme);
 
   return (
     <section>
       <img src="/logo.png" alt="Intro Logo" className={classes.logo}/>
       <Container maxWidth={false}>
-        <div className={classes.typoRoot}>
+      <ThemeProvider theme={theme}>
           <Typography variant="h1" className={classes.typo}>
             Welcome to Intro
           </Typography>
@@ -37,7 +39,7 @@ export default function Index() {
           <Typography variant="h5" className={classes.typo}>
             페이지 준비중입니다.
           </Typography>
-        </div>
+        </ThemeProvider>
         <ContactForm />
         <footer>
         </footer>
