8020baef1669db75f9ddd897e44e09a9c2cc1aab
intro-taktak <tak@introsolution.kr>
2021-05-18 17:07:26 +0900

    메타데이터 추가

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import MuiLink from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
+import Head from 'next/head';
 
 const useStyles = makeStyles((theme) => ({
   
@@ -76,6 +77,15 @@ export default function Index() {
 
   return (
     <section>
+      <Head>
+          <meta name="description" content="We turn your dream into reality"/>
+          <meta property="og:type" content="website"/>
+          <meta property="og:title" content="인트로"/>
+          <meta property="og:description" content="Design, Solution, Consilience: INTRO"/>
+          <meta property="og:image" src="wst.png"/>
+          <meta property="og:url" content="https://www.introsolution.kr"/>
+          <link rel="canonical" href="https://www.introsolution.kr"></link>
+      </Head>
         <CardMedia className={classes.jumbotron} image="/ux-store-jJT2r2n7lYA-unsplash.jpg">
           <img src="/logo.svg" alt="Intro Logo" className={classes.logo} />
           <div className={classes.jumbotronDiv}>
